290cba374f4dad242c6e20bead0ccc85cfe5e489
Hanse2408 <hansamalhettiwatte@gmail.com>
2026-04-24 14:21:49 +0530

    refactor: restructure backend into shared operations hub package layout

--- backend/src/main/java/com/smartcampus/operationshub/booking/package-info.java
+++ backend/src/main/java/com/smartcampus/operationshub/booking/package-info.java
@@ -1,7 +1,9 @@
 /**
  * Booking module – reserved for future implementation.
  *
- * <p>This package will house all classes for the room/resource booking
- * feature (controllers, services, repositories, DTOs, entities).</p>
+ * <p>
+ * This package will house all classes for the room/resource booking
+ * feature (controllers, services, repositories, DTOs, entities).
+ * </p>
  */
 package com.smartcampus.operationshub.booking;
