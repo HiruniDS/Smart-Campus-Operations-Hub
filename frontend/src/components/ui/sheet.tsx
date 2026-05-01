40a48c5491731a11466e8d20d2d3da9a72ac0274
Hanse2408 <hansamalhettiwatte@gmail.com>
2026-05-01 16:45:00 +0530

    chore: frontend cleanup pass - remove orphaned files, untrack build artifacts, fix TS lint

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -9,7 +9,7 @@ function Sheet({ ...props }: SheetPrimitive.Root.Props) {
   return <SheetPrimitive.Root data-slot="sheet" {...props} />
 }
 
-function SheetTrigger({ ...props }: SheetPrimitive.Trigger.Props) {
+function SheetTrigger({ asChild: _asChild, ...props }: SheetPrimitive.Trigger.Props & { asChild?: boolean }) {
   return <SheetPrimitive.Trigger data-slot="sheet-trigger" {...props} />
 }
 
